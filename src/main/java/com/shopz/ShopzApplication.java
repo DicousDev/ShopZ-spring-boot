fa9e82b41e940df8f5cf72f5350be124549c53f5
João Victor Duarte <joaodicu387@gmail.com>
2022-04-26 21:14:22 -0300

    Criando projeto

---
+++ src/main/java/com/shopz/ShopzApplication.java
@@ -0,0 +1,13 @@
+package com.shopz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopz/ShopzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
