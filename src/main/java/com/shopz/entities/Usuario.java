88a2b5bd857b05bfdcbbfef4b60f8ebea77539fe
João Victor Duarte <joaodicu387@gmail.com>
2022-04-27 23:35:23 -0300

    Associando produtos no usuario

--- src/main/java/com/shopz/entities/Usuario.java
+++ src/main/java/com/shopz/entities/Usuario.java
@@ -1,9 +1,13 @@
 package com.shopz.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.shopz.dto.CreateUsuarioRequest;
@@ -18,6 +22,9 @@ public class Usuario {
 	private String cpf;
 	private String email;
 	private String password;
+	
+	@ManyToMany
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Usuario() {
 		
