74b1e7188066980d03294afff44c46c953cdbac0
João Victor Duarte <joaodicu387@gmail.com>
2022-05-02 22:28:59 -0300

    Adicionando mais produtos ao banco

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,5 @@
-INSERT INTO tb_product(titulo, preco_atual, parcelas) VALUES ('Camiseta Black Heart', 74.99, 4);
+INSERT INTO tb_product(titulo, preco_atual, parcelas) VALUES ('Camiseta Black Heart', 39.90, 4);
+INSERT INTO tb_product(titulo, preco_atual, parcelas) VALUES ('Camiseta O Auto da Compadecida', 39.90, 4);
+INSERT INTO tb_product(titulo, preco_atual, parcelas) VALUES ('Camiseta Paulo Freire', 39.90, 4);
+INSERT INTO tb_product(titulo, preco_atual, parcelas) VALUES ('Camiseta Cafezim', 39.90, 4);
+INSERT INTO tb_product(titulo, preco_atual, parcelas) VALUES ('Camiseta Mapa Brasilidade', 39.90, 4);
